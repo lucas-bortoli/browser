4d630e9b21098433bf9a10ca8a6c61f59b8df7d2
pantheonlbs <8a16b@protonmail.com>
2020-11-08 18:03:16 -0300

    Webview focus fix/ remove context menus

--- src/js/page.js
+++ src/js/page.js
@@ -1,72 +1,11 @@
-const { clipboard, ipcRenderer } = require('electron')
-
-const ContextMenu = require('./classes/context_menu')
-const UI = require('./ui/ui')
-
 const webView = document.querySelector('#content')
 
-let ctx_menu = null
-
 webView.addEventListener('ipc-message', e => {
-    if (e.channel === 'webview-context-menu-open') {
-        let data = e.args[0]
+    const event_name = e.channel
+})
 
-        if (ctx_menu) 
-            ctx_menu.close()
-
-        let menu_elements = []
-
-        for (let target of data.targets) {
-            if (target.type === 'link') {
-                menu_elements.push({
-                    icon: 'content_copy',
-                    label: 'Copy link',
-                    click: () => clipboard.writeText(target.url)
-                })
-            } else if (target.type === 'image') {
-                menu_elements.push({
-                    icon: 'content_copy',
-                    label: 'Copy image link',
-                    click: () => clipboard.writeText(target.url)
-                })
-            } else if (target.type === 'input') {
-                menu_elements.push({
-                    icon: 'content_copy',
-                    label: 'Copy selection',
-                    disabled: !target.canCopy,
-                    click: () => webView.send('context-menu-copy')
-                })
-
-                menu_elements.push({
-                    icon: 'content_paste',
-                    label: 'Paste',
-                    click: () => webView.send('context-menu-paste')
-                })
-            } else {
-                if (target.canCopy) {
-                    menu_elements.push({
-                        icon: 'content_copy',
-                        label: 'Copy selection',
-                        disabled: !target.canCopy,
-                        click: () => webView.send('context-menu-copy')
-                    })
-                }
-            }
-        }
-
-        menu_elements.push({
-            icon: "code",
-            label: "Inspect element",
-            click: () => webView.inspectElement(data.x, data.y)
-        })
-
-        ctx_menu = new ContextMenu(data.x, data.y, menu_elements)
-
-        UI.open_bar()
-    } else if (e.channel === 'webview-context-menu-close') {
-        if (ctx_menu) 
-            ctx_menu.close()
-
-        ctx_menu = null
-    }
+webView.addEventListener("dom-ready", event => {
+    // Remove this once https://github.com/electron/electron/issues/14474 is fixed
+    webView.blur()
+    webView.focus()
 })
